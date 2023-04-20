35f0244a40223add74690b2eaf06cef6a282cdfb
nidroy <nidroy@mail.ru>
2023-04-20 12:17:19 +0300

    adding a client model

---
+++ Models/Client.cs
@@ -0,0 +1,13 @@
+﻿namespace Test_task.Models
+{
+    public class Client
+    {
+        public int Id { get; set; } // идентификатор
+        public string FirstName { get; set; } = string.Empty; // имя
+        public string LastName { get; set; } = string.Empty; // фамилия
+        public string DateOfBirth { get; set; } = string.Empty; // дата рождения
+        public string Email { get; set; } = string.Empty; // электронная почта
+        public string PhoneNumber { get; set; } = string.Empty; // номер телефона
+        public string Address { get; set; } = string.Empty; // адрес
+    }
+}
